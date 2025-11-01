aa61a079d7ac3b455e8f7119583ca33d85d019dc
TheRABU <mdfazlerabbi114@gmail.com>
2025-11-01 13:59:39 +0600

    added react router, pages

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <>
+      <div className="min-h-screen bg-neutral-800">
+        <h2 className="text-3xl text-white text-center">Hellooo home page</h2>
+      </div>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/routes/AllRoutes.tsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter, RouterProvider } from "react-router";
+import RootLayout from "../layouts/RootLayout";
+import Home from "../pages/home/Home";
+
+const AllRoutes = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <RootLayout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+};
+
+export default AllRoutes;
